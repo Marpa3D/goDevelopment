24d423a45ef143c20eb884175897c492253e0794
Viacheslav <Marpa3d@yandex.ru>
2023-09-17 17:16:18 +0300

    Arrays in Go

--- src/AlgoAndDataStruct/DataStruct/Arrays/main.go
+++ src/AlgoAndDataStruct/DataStruct/Arrays/main.go
@@ -6,4 +6,7 @@ import "fmt"
 func main() {
 	fmt.Println("Arrays")
 
+	var someArray [5]int
+
+	fmt.Printf("Адрес массива: %p\nЗначения в массиве: %+v\nТип %T\n", &someArray, someArray, someArray)
 }
---
+++ src/Type_assertion/main.go
@@ -0,0 +1,32 @@
+// Оператор type assertion
+package main
+
+import "fmt"
+
+func printType(t interface{}) {
+	switch t.(type) {
+	case int:
+		fmt.Println("int")
+	case float32:
+		fmt.Println("float32")
+	case float64:
+		fmt.Println("float64")
+	case string:
+		fmt.Println("string")
+	case bool:
+		fmt.Println("bool")
+	default:
+		fmt.Println("unknown")
+	}
+}
+
+func main() {
+	fmt.Println("opertator type assertion")
+
+	printType("Это строка")
+	printType(true)
+	printType(88)
+	printType(14.8)
+	printType('c')
+
+}
