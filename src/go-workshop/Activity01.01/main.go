4416c7b9d6340c97b3ccd5cfb634a5fe28be3cd1
Viacheslav <Marpa3d@yandex.ru>
2023-09-25 08:00:49 +0300

    Изменил название директории intro-3 на Activity01.01

---
+++ src/go-workshop/Activity01.01/main.go
@@ -0,0 +1,14 @@
+// мМедицинская форма для кабинета врача
+package main
+
+import "fmt"
+
+func main() {
+
+	var name, lastName string
+	var age int
+	var allergy bool
+
+	fmt.Println(name, lastName, age, allergy)
+
+}
