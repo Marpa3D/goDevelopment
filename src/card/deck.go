17a6f2d5676b995cb502957cb36b16758e5cdc92
Viacheslav <Marpa3d@yandex.ru>
2023-10-12 16:24:28 +0300

    Добавлена функция создания колоÐдÐы карт и провеÐден рефакторинг кода

--- src/card/deck.go
+++ src/card/deck.go
@@ -1,4 +1,26 @@
 package main
 
+import "fmt"
+
 // Создание типа "Колода" (Deck). Это слайс типа string
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardsSuits := []string{"Пик", "Бубен", "Червей", "Треф"}
+	cardValues := []string{"Туз", "Двойка", "Тройка", "Четверка"}
+
+	for _, suits := range cardsSuits {
+		for _, values := range cardValues {
+			cards = append(cards, values+" "+suits)
+		}
+	}
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- src/card/main.go
+++ src/card/main.go
@@ -1,16 +1,8 @@
 // ира "Карты"
 package main
 
-import "fmt"
-
 func main() {
-	cards := deck{"Ace of Diamonds", newCard(), "Six of Spades"}
+	cards := newDeck()
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Ace of Spades"
+	cards.print()
 }
