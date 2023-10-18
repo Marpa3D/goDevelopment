8db8aa6d46f781b5a4c6e2697993cbff82f41626
Viacheslav "Marpa3D" Sharov <Marpa3d@yandex.ru>
2023-10-19 01:15:52 +0300

    Рефакторинг

--- src/web-go/scanPorts/main.go
+++ src/web-go/scanPorts/main.go
@@ -1,6 +1,33 @@
 // Сканер TCP портов
 package main
 
+import (
+	"fmt"
+	"net"
+	"sync"
+)
+
+// TCPscan - многопоточный сканер TCP портов
+func TCPscan() {
+	var wg sync.WaitGroup
+	for i := 1; i < 1024; i++ {
+		wg.Add(1)
+		go func(j int) {
+			defer wg.Done()
+			addr := fmt.Sprintf("scanme.nmap.org:%d", j)
+			connect, err := net.Dial("tcp", addr)
+			if err != nil {
+				// порт закрыт или отфильтрован брендмауэром
+				fmt.Printf("Порт %d закрыт или отфильтрован\n", j)
+				return
+			}
+			connect.Close()
+			fmt.Printf("Порт %v открыт\n", connect)
+		}(i)
+	}
+	wg.Wait()
+}
+
 func main() {
-	tcpScan()
+	TCPscan()
 }
